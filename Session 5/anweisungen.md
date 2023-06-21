397734b3d6a1d0e047d35f68fd012e34898140fa
Vogt <Andi.Vogt@telekom.de>
2023-06-21 13:26:57 +0200

    Session 5

---
+++ Session 5/anweisungen.md
@@ -0,0 +1,3 @@
+# Aufgabe:
+1. Erstelle eine Schleife.
+2. In dieser Schleife wird gefragt wie Alt der Benutzer ist
---
+++ Session 5/schleifen.py
@@ -0,0 +1,15 @@
+# for 
+# while
+
+
+liste = [1,2,3,4,5,6]
+
+for zahl in liste:
+    print(zahl)
+    zahl2 = zahl + 50
+    print(zahl2)
+    
+
+zaehler = 0
+while zaehler < 100:
+    print(zaehler)
